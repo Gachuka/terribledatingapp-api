9a9aed93297b19ea647717db104b0ff8202f22dd
Li Yu <gachuka@gmail.com>
2022-10-15 16:37:13 -0400

    try

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   console.log('GET Request')
   knex('user')
-    .select('user', 'question_1', 'question_2', 'question_3', 'question_4', 'question_5', 'question_6', 'question_7', 'question_8', 'question_9', 'question_10', 'question_11', 'question_12', 'question_13', 'question_14', 'question_15', 'question_16', 'question_17', 'question_18', 'question_19', 'question_20', 'question_21')
+    .select('*')
     .then((data) => {
       res.status(200).json(data);
     })
