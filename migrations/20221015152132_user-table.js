166e69edeb8ffaa7d0b46009806f87b3761f4177
Li Yu <gachuka@gmail.com>
2022-10-15 11:32:38 -0400

    Create user table migrate

---
+++ migrations/20221015152132_user-table.js
@@ -0,0 +1,33 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('user', (table) => {
+      table.increments('id').primary();
+      table.string('user').notNullable();
+      table.boolean('question_1').notNullable();
+      table.boolean('question_2').notNullable();
+      table.boolean('question_3').notNullable();
+      table.boolean('question_4').notNullable();
+      table.boolean('question_5').notNullable();
+      table.boolean('question_6').notNullable();
+      table.boolean('question_7').notNullable();
+      table.boolean('question_8').notNullable();
+      table.boolean('question_9').notNullable();
+      table.boolean('question_10').notNullable();
+      table.boolean('question_11').notNullable();
+      table.boolean('question_12').notNullable();
+      table.boolean('question_13').notNullable();
+      table.boolean('question_14').notNullable();
+      table.boolean('question_15').notNullable();
+      table.boolean('question_16').notNullable();
+      table.boolean('question_17').notNullable();
+      table.boolean('question_18').notNullable();
+      table.boolean('question_19').notNullable();
+      table.boolean('question_20').notNullable();
+      table.boolean('question_21').notNullable();
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('save');
+};
